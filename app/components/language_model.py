1e3ea69373cd53eca3d4dbeb72eb7291eca57fc7
Bassim E <bassime@bassims-air.lan>
2023-12-07 15:26:52 -0800

    some style changes and redundant code removal

--- app/components/language_model.py
+++ app/components/language_model.py
@@ -5,10 +5,5 @@ from llama_index.llms.base import ChatMessage, MessageRole, CompletionResponse
 class LanguageModel:
     def __init__(self):
         # Initialize the language model
-        self.llm = Vertex(model="chat-bison", temperature=0,  # change to chat bison later on
+        self.llm = Vertex(model="chat-bison", temperature=0,
                           additional_kwargs={})
-
-    def generate_answer(self, query):
-        # Generate a response to the query
-        response = self.llm.complete(query).text
-        return response
