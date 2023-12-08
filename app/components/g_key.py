7b5f7d1341db0f1cc79c63a238f02bf3bdbb556e
Fadil Eledath <fadile@dmcinfo.com>
2023-12-08 02:43:01 -0600

    Used secrets to write cred file

---
+++ app/components/g_key.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import json
+
+def write_file():
+    key_path = st.secrets["JSON_PATH"]
+    with open(key_path, "w+") as f:
+        creds = {
+            "type": st.secrets["type"],
+            "project_id": st.secrets["project_id"],
+            "private_key_id": st.secrets["private_key_id"],
+            "private_key": st.secrets["private_key"],
+            "client_email": st.secrets["client_email"],
+            "client_id": st.secrets["client_id"],
+            "auth_uri": st.secrets["auth_uri"],
+            "token_uri": st.secrets["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+            "universe_domain": st.secrets["universe_domain"]
+        }
+        json.dump(creds, f)
