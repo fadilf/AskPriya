f2ef3cc1a04b5d8ec5b7287a04f685f383ce8e0c
Fadil Eledath <fadile@dmcinfo.com>
2023-12-08 02:54:53 -0600

    added g_key cred write to initialization

--- app/components/initialize_services.py
+++ app/components/initialize_services.py
@@ -2,11 +2,12 @@ import os
 import streamlit as st
 import google.auth
 from llama_index import StorageContext, load_index_from_storage
-
+from g_key import write_file
 
 @st.cache_resource
 def initialize_services():
     # Authenticate with Google Cloud
+    write_file()
     key_path = st.secrets["JSON_PATH"]
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = key_path
     credentials, project_id = google.auth.default()
